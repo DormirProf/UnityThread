d6f510415cd936b8e1d20ce70fed86955b5866b8
DormirProf <zheny.garshedsky1@gmail.com>
2021-06-22 15:07:14 +0300

    Add

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # UnityThread
-Using unity elements from any threads
+Using unity elements from any threads  
+  
+This script must be placed on any included object in the unity.  
+  
+Usage in any method:    
+  
+    SyncContext.RunOnUnityThread ( ( ) => {  
+        //code  
+    }) ;  
+  
+Данный скрип нужно поместить на любой включенный объект в юнити.
+  
+Использование в любом методе:    
+  
+    SyncContext.RunOnUnityThread ( ( ) => {  
+        //code  
+    }) ;  
---
+++ SyncContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class SyncContext : MonoBehaviour
+{
+    public static TaskScheduler unityTaskScheduler;
+    public static int unityThread;
+    public static SynchronizationContext unitySynchronizationContext;
+    static public Queue<Action> runInUpdate = new Queue<Action>();
+
+    public static bool isOnUnityThread => unityThread == Thread.CurrentThread.ManagedThreadId;
+
+
+    private void Awake()
+    {
+        unitySynchronizationContext = SynchronizationContext.Current;
+        unityThread = Thread.CurrentThread.ManagedThreadId;
+        unityTaskScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+    }
+    
+    private void Update()
+    {
+        while (runInUpdate.Count > 0)
+        {
+            Action action = null;
+            lock (runInUpdate)
+            {
+                if (runInUpdate.Count > 0)
+                    action = runInUpdate.Dequeue();
+            }
+
+            action?.Invoke();
+        }
+    }
+    
+    public static void RunOnUnityThread(Action action)
+    {
+        if (unityThread == Thread.CurrentThread.ManagedThreadId)
+        {
+            action();
+        }
+        else
+        {
+            lock (runInUpdate)
+            {
+                runInUpdate.Enqueue(action);
+            }
+
+        }
+    }
+}
